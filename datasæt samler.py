eee17a7ff4db81e2a90e7735663f76ea5129b233
alfredksigsgaard <alfredksigsgaard@gmail.com>
2024-04-30 08:39:15 +0200

    dinmorergrim

---
+++ datasæt samler.py
@@ -0,0 +1,32 @@
+import pandas as pd
+brand = ["audi","bmw","ford","hyundai","mercedes","skoda","toyota","vauxhall","vw"]
+def brandtilfil(brand):
+    df = pd.read_csv(f"data\{brand}.csv", 
+                    sep = ';', 
+                    engine = 'python')
+    word_to_add = brand
+    df['brand'] = word_to_add
+
+    df.to_csv(f"datany\{brand}ny.csv", 
+                    sep = ';', )
+    
+for x in brand:
+    brandtilfil(x)
+
+columns = ['model', 'year', 'price', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg', 'engineSize', 'brand']
+dfsaml = pd.DataFrame()
+
+def datasaml(brand):
+    global dfsaml
+    dfmerge = pd.read_csv(f"datany\{brand}ny.csv", 
+                    sep = ';', 
+                    engine = 'python')
+    
+    dfsaml = pd.concat([dfsaml, dfmerge])
+    
+
+for x in brand:
+    datasaml(x)
+dfsaml=dfsaml[columns]
+dfsaml.to_csv("samletdata.csv", 
+                    sep = ';', )
