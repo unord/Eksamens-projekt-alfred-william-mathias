78b928ba478495f657efadcd42b9dfed9d8d9622
mathias <selandermathias00@gmail.com>
2024-05-16 19:49:09 +0200

    flask <- 🤓

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,24 @@
 from flask import render_template, redirect, session, url_for
 from app import app
 from app.forms import DataInput
+import pickle as pkl
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import pandas as pd
+
+preprocessor = pkl.load(open("preprocessor.pkl", "rb"))
+model = pkl.load(open("model.pkl", "rb"))
 
 @app.route('/', methods=["GET", "POST"])
 @app.route('/index', methods=["GET", "POST"])
 def index():
     form = DataInput()
-    data = ""
+    pred_price = None
     if form.validate_on_submit():
-        data = {
-            "brand": form.brand.data,
-            "model": form.model.data,
-            "engine": form.engine.data,
-            "year": form.y.data,
-            "km": form.km.data,
-            "gear": form.gear.data,
-            "fuel": form.fuel.data,
-            "fe": form.fe.data
-        }
+        pred_data = {col: [data] for col, data in zip(form.data.keys(), [data if isinstance(data, str) else float(data) for data in form.data.values()])}
+        pred_data = pd.DataFrame(pred_data)
+        pred_data = preprocessor.transform(pred_data)
+        pred_price = round(model.predict(pred_data)[0])
         redirect("/")
-    return render_template("index.html", title="Used car pricepicker", form=form, data=data)
+    return render_template("index.html", title="Used car pricepicker", form=form, price=pred_price)
