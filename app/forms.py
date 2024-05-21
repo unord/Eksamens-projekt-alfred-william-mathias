c971e8351a658f1cb033713e4ed453c7b5ce1846
mativ <selandermathias00@gmail.com>
2024-05-21 11:52:25 +0200

    william er en 🤓

--- app/forms.py
+++ app/forms.py
@@ -5,9 +5,9 @@ from wtforms.validators import DataRequired
 class DataInput(FlaskForm):
     model = StringField("Model", validators=[DataRequired()])
     year = DecimalField("Year", validators=[DataRequired()])
-    transmission = RadioField("Transmission", choices=[("Manual", "Manual"), ("Automatic", "Automatic")], validators=[DataRequired()])
+    transmission = RadioField("Transmission", choices=[("Manual", "Manual"), ("Automatic", "Automatic"), ("Semi-Auto", "Semi-Auto"), ("Other", "Other")], validators=[DataRequired()])
     mileage = DecimalField("Mileage", validators=[DataRequired()])
-    fuelType = RadioField("Fuel type", choices=[("Petrol" ,"Petrol"), ("Diesel", "Diesel")], validators=[DataRequired()])
+    fuelType = RadioField("Fuel type", choices=[("Petrol" ,"Petrol"), ("Diesel", "Diesel"), ("Hybrid", "Hybrid"), ("Other", "Other"), ("Electric", "Electric")], validators=[DataRequired()])
     mpg = DecimalField("MPG (miles per gallon)", validators=[DataRequired()])
     engineSize = DecimalField("Engine size", validators=[DataRequired()])
     brand = SelectField("Brand", choices=[("audi", "Audi"), ("bmw", "BMW"), ("ford", "Ford"), ("hyundai", "Hyundai"), ("Mercedes", "Mercedes"), ("skoda", "Skoda"), ("toyota", "Toyota"), ("vauxhall", "Vauxhall"), ("vw", "VW")], validators=[DataRequired()])
